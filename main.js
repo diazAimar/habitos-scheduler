c7097891efef88eea5789c82ad8ffc46582ee8e7
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2021-05-12 22:23:02 -0300

    Agregada modularizacion de funcion. Corregido error que borraba siempre el primer habito de la lista, aunque se clickee otro. Agregado mensaje de advertencia al intentar ingresar un habito con la tecla "ENTER". Elimnados comentarios extra.

--- main.js
+++ main.js
@@ -17,19 +17,49 @@ const habitosContainer = document.querySelector(".container--habitos");
 /* Agregar o quitar habitos */
 const agregarHabitos = document.querySelector(".agregar--habitos");
 const quitarHabitos = document.querySelector(".quitar--habitos");
-
+/* Cerrar mensaje */
+const cerrarMensaje = document.querySelector(".btn-cerrar-mensaje");
 /* Elemento de todos los botones */
 const formIcons = document.querySelectorAll(".form-icon");
+/* Form habito */
+const formHabito = document.querySelector(".form--habito");
+/* Basurita */
+const borrarHabito = document.querySelector(".borrar--habito");
+
 
 /* Event Handlers */
+formPlusIcon.addEventListener('click', agregarHabito);
+formMinusIcon.addEventListener('click', agregarHabito);
+formEqualsIcon.addEventListener('click', agregarHabito);
+
+/* removeBtnListener, funcion para eliminar un elemento */
+const removeBtnListener = (e) => {
+    let habitoEncontrado = false;
+    let indexHabitoEncontrado;
+    let i = 0;
+    let habitsArray = localStorage.getItem("arr");
+    let idHabitoActual = e.currentTarget.parentNode?.id;
+    console.log(e.currentTarget.parentNode);
+    console.log(e.currentTarget.parentNode.id);
+    console.log(idHabitoActual)
+    habitsArray = JSON.parse(localStorage.getItem("arr"));
+    console.log(habitsArray);
+    let arrayNuevo = habitsArray.filter(habit => habit.id != idHabitoActual);
+    console.log(arrayNuevo);
+    e.currentTarget.parentNode?.remove();
+/*     console.log(habitsArray);
+ */    localStorage.setItem("arr", JSON.stringify(arrayNuevo));
+}
 
 /* function llamada agregarHabito, que dependiendo del icono que se usa, crea un habito con ese tipo y color */
-
 function agregarHabito(e){
     if (formInput.value) {
         localStorage.getItem("arr");
+        /* Guardo la fecha actual para usar como id unico */
         let date = Date.now();
+        /* Busco el tipo de habito que voy a agregar */
         let tipo = e.currentTarget.classList.item(2);
+        /* Creo un archivo html que se elimina una vez termina la funcion */
         const doc = document.implementation.createHTMLDocument()
         doc.body.innerHTML = `
             <div class ="habits habits--${tipo}" id="${date}">
@@ -37,25 +67,11 @@ function agregarHabito(e){
                 <button><i class="far fa-trash-alt"></i></button>
             </div>
         `;
+        /* selecciono el parrafo, le cambio el textContent por formInput.value, el valor que se encuentra en el input */
         doc.querySelector('p').textContent = formInput.value
-        doc.querySelector('button').addEventListener('click', (e) => {
-            let habitoEncontrado = false;
-            let indexHabitoEncontrado;
-            let i = 0;
-            let habitsArray = localStorage.getItem("arr");
-            e.currentTarget.parentNode?.remove();
-            let dateHabitoActual = e.currentTarget.parentNode?.id;
-            habitsArray = JSON.parse(localStorage.getItem("arr"));
-            do {
-                if(habitsArray[i]["id"] == dateHabitoActual) {
-                    indexHabitoEncontrado = i;
-                    habitoEncontrado = true;
-                }
-                i++;
-            } while (habitoEncontrado == false && i < habitsArray.length);
-            habitsArray.splice(habitsArray[indexHabitoEncontrado], 1);
-            localStorage.setItem("arr", JSON.stringify(habitsArray));
-        }); 
+        /* selecciono el boton y le pongo un eventlistener, caso que se haga click en el se ejecutara la funcion removeBtnListener, que elimina el elemento  */
+        doc.querySelector('button').addEventListener('click', removeBtnListener);
+        /* doc.body.children[0] would be the div element I added with the innerHTML, e.currentTarget in the event would be the button that was clicked so the parentNode would be its parent, (so the div) */
         habitosContainer.append(doc.body.children[0]);
         /* Que significa || []: F variable is not defined (or has a falsey value) THEN set it to an empty object. */
         /* Hago un parse del item tipo y si no existe creo el arr vacio */
@@ -69,14 +85,10 @@ function agregarHabito(e){
         /* Al array le voy a pushear el objeto que tengo */
         arrObjHabitos.push(objHabito);
         /* Guardo en localStorage el array convertido a string */
-        localStorage.getItem("arr");
         localStorage.setItem("arr", JSON.stringify(arrObjHabitos));
         formInput.value = '';
     }
 };
-formPlusIcon.addEventListener('click', agregarHabito);
-formMinusIcon.addEventListener('click', agregarHabito);
-formEqualsIcon.addEventListener('click', agregarHabito);
 
 function cargarHabitos(){
     let arrHabitosAll = JSON.parse(localStorage.getItem("arr"));
@@ -87,121 +99,45 @@ function cargarHabitos(){
             let id = arrHabitosAll[i]["id"];
             const doc = document.implementation.createHTMLDocument()
             doc.body.innerHTML = `
-                <div class ="habits habits--${tipo} id="${id}">
+                <div class ="habits habits--${tipo}" id="${id}">
                     <p></p>
                     <button><i class="far fa-trash-alt"></i></button>
                 </div>
             `;
             doc.querySelector('p').textContent = valor;
-            doc.querySelector('button').addEventListener('click', (e) => {
-                let habitoEncontrado = false;
-                let indexHabitoEncontrado;
-                let i = 0;
-                let habitsArray = localStorage.getItem("arr");
-                e.currentTarget.parentNode?.remove();
-                let dateHabitoActual = e.currentTarget.parentNode?.id;
-                habitsArray = JSON.parse(localStorage.getItem("arr"));
-                do {
-                    if(habitsArray[i]["id"] == dateHabitoActual) {
-                        indexHabitoEncontrado = i;
-                        habitoEncontrado = true;
-                    }
-                    i++;
-                } while (habitoEncontrado == false && i < habitsArray.length);
-                habitsArray.splice(habitsArray[indexHabitoEncontrado], 1);
-                localStorage.setItem("arr", JSON.stringify(habitsArray));
-            }); 
+            doc.querySelector('button').addEventListener('click', removeBtnListener);
             habitosContainer.append(doc.body.children[0]);
         }
     }
 }
 cargarHabitos();
-/* 
-OTRA FORMA
-<i data-icon-type="plus" class="fas fa-plus plus"></i>
-<i data-icon-type="minus" class="fas fa-minus minus"></i>
-<i data-icon-type="equals" class="fas fa-equals equals"></i>
-
-document.addEventListener('click', (e) => {
-  const el = e.target;
-  if (el.hasAttribute('data-icon-type') {
-    const type = el.dataset.iconType; // this would equal one of the values
-    // your code based on which icon was clicked would go here
-  }
-}); */
-
-/* AGREGAR HABITO VERDE */
-/* formPlusIcon.addEventListener("click", function(e){
-    if (formInput.value) {
-        const doc = document.implementation.createHTMLDocument()
-        doc.body.innerHTML = `
-            <div class ="habits habits--plus">
-                <p></p>
-                <button><i class="far fa-trash-alt"></i></button>
-            </div>
-        `;
-        doc.querySelector('p').textContent = formInput.value
-        doc.querySelector('button').addEventListener('click', (e) => {
-          e.currentTarget.parentNode?.remove()
-        })
-        habitosContainer.append(doc.body.children[0]);
-        formInput.value = '';
-    }
-}); */
-
-/* AGREGAR HABITO ROJO */
-/* formMinusIcon.addEventListener("click", function(e){
-    if (formInput.value) {
-        const doc = document.implementation.createHTMLDocument()
-        doc.body.innerHTML = `
-            <div class ="habits habits--minus">
-                <p></p>
-                <button><i class="far fa-trash-alt"></i></button>
-            </div>
-        `;
-        doc.querySelector('p').textContent = formInput.value
-        doc.querySelector('button').addEventListener('click', (e) => {
-          e.currentTarget.parentNode?.remove()
-        })
-        habitosContainer.append(doc.body.children[0]);
-        formInput.value = '';
-    }
-}); */
-
-/* AGREGAR HABITO AMARILLO */
-/* formEqualsIcon.addEventListener("click", function(e){
-    if (formInput.value) { */
-        /* Creo un archivo html que se elimina una vez termina la funcion */
-        // const doc = document.implementation.createHTMLDocument()
-        /* el cuerpo del nuevo doc html */
-/*         doc.body.innerHTML = `
-            <div class ="habits habits--equals">
-                <p></p>
-                <button><i class="far fa-trash-alt"></i></button>
-            </div>
-        `; */
-        /* doc.body.children[0] would be the div element I added with the innerHTML, e.currentTarget in the event would be the button that was clicked so the parentNode would be its parent, (so the div) */
-        /* selecciono el parrafo, le cambio el textContent por formInput.value */
-        // doc.querySelector('p').textContent = formInput.value
-        /* selecciono el boton y le pongo un eventlistener */
-        // doc.querySelector('button').addEventListener('click', (e) => {
-            /* e.currentTarget, agarra al elemento que clickie cuando corrio el evento, en este caso el boton de "trashcan" */
-            /* si existe el padre (que seria el div completo), lo remuevo */
-/*           e.currentTarget.parentNode?.remove()
-        })
-        habitosContainer.append(doc.body.children[0]);
-        formInput.value = '';
-    }
-}); */
-
 
 /* BORRAR TODOS LOS HABITOS */
 quitarHabitos.addEventListener("click", function(arrObjHabitos){
     habitosContainer.innerHTML = "";
     localStorage.clear();
-})
+});
 
 /* FOCUS TEXT INPUT */
 agregarHabitos.addEventListener("click", function(){
     formInput.focus();
 });
+
+/* MENSAJE DE ADVERTENCIA */
+formInput.addEventListener('keypress', function(e){
+    if (e.key === "Enter"){
+        e.preventDefault();
+        formInput.value = "Ingrese un habito y agreguelo con el boton correspondiente ->";
+        const mensaje = document.createElement("div");
+        mensaje.innerHTML = "<button class='btn btn-cerrar-mensaje'> Limpiar Mensaje</button>";
+        formHabito.append(mensaje);
+    }
+});
+
+if(cerrarMensaje){
+    cerrarMensaje.addEventListener('click', function(e){
+        e.preventDefault();
+        formInput.value = "";
+        mensaje.remove();
+    });
+}
